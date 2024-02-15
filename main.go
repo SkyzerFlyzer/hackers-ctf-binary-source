11b1e5912ed65962df1c90e1a4c644d026de873b
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-15 11:41:11 +0000

    Added joey and izzy

--- main.go
+++ main.go
@@ -22,6 +22,10 @@ func printBestPerson(bestPerson string) {
 		fmt.Println("I mean I agree but sucking up to the creator of this program won't get you anywhere")
 	} else if bestPerson == "molly" {
 		fmt.Println("I take back what I said about Joe, Molly is the best person in this room, still doesn't mean you get a hint")
+	} else if bestPerson == "joey" {
+		fmt.Println("Joey may be the current chairperson but not even he knows this flag!")
+	} else if bestPerson == "izzy" {
+		fmt.Println("I didn't think anyone would think Izzy was the best person, unless you have a crush on her of course, but I guess you can have a flag she would like dmu_hackers{IzzyIsTheBest}. It won't work but you can have it")
 	} else {
 		fmt.Println(bestPerson, "is the best person in this room")
 	}
