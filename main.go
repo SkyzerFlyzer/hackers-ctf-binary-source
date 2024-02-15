91ae7f6efc3a6980fb82f04f36b3671c2a8b3852
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-15 11:32:40 +0000

    Separated output and input

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
-func getUserInput() {
+func getUserInput() string {
 	fmt.Println("Who is the best person in this room?")
 	var input string
 	_, err := fmt.Scanln(&input)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return ""
 	}
-	fmt.Println("You think", input, "is the best person in this room.")
+	return input
+}
+
+func printBestPerson(bestPerson string) {
+	fmt.Println(bestPerson, "is the best person in this room")
 }
 
 func main() {
-	getUserInput()
+	bestPerson := getUserInput()
+	printBestPerson(bestPerson)
 }
