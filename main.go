2392cf171c0e9748cb9649d8556a0f5170fd8f73
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-15 11:48:37 +0000

    Added a few more names

--- main.go
+++ main.go
@@ -27,6 +27,18 @@ func printBestPerson(bestPerson string) {
 		fmt.Println("Joey may be the current chairperson but not even he knows this flag!")
 	case "izzy":
 		fmt.Println("I didn't think anyone would think Izzy was the best person, unless you have a crush on her of course, but I guess you can have a flag she would like dmu_hackers{IzzyIsTheBest}. It won't work but you can have it")
+	case "orlin":
+		fmt.Println("I didn't know Maisie was in this room. Live orlin update anyone?")
+	case "maisie":
+		fmt.Println("Didn't think Maisie was here, but I guess you're just trying to be Orlin")
+	case "dan":
+		fmt.Println("Either you're looking for miso or DJ, either way, you're not getting a flag")
+	case "miso":
+		fmt.Println("Thank you for specifying the correct Dan. But no flag for you")
+	case "dj":
+		fmt.Println("Thank you for specifying the correct Dan. But no points for being named Emma")
+	case "emma":
+		fmt.Println("Didn't think Emma came to hacker's sessions")
 	default:
 		fmt.Println(bestPerson, "is the best person in this room")
 	}
