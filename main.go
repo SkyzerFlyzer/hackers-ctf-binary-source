aaab04e9c09b708e07e6323f1290d82ba243693e
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-15 11:31:15 +0000

    Basic user input

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func getUserInput() {
+	fmt.Println("Who is the best person in this room?")
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("You think", input, "is the best person in this room.")
+}
+
+func main() {
+	getUserInput()
 }
